5257a0c4589157c798e615fb03551b9263178324
eperez <eperez@nic.cl>
2025-02-19 14:03:38 -0300

    style: enhance BannersPromo component layout and styling for improved visual hierarchy and responsiveness

--- src/components/BannersPromo.tsx
+++ src/components/BannersPromo.tsx
@@ -1,54 +1,59 @@
 import '../styles/BannersPromo.css'
 
 interface BannerProps {
-    image: string;
-    discount: string;
-    label: string;
-    title: string;
-   }
-   
-   const Banner: React.FC<BannerProps> = ({ image, discount, label, title }) => {
-    return (
-      <div className="banner">
-        <img src={image} alt={title} />
-        <div className="banner-content-promo">
-          <div className="discount">{discount}</div>
-          <div className="label">{label}</div>
-          <div className="title">{title}</div>
-        </div>
-      </div>
-    );
-   };
-   
-   const BannerContainer: React.FC = () => {
-    const banners = [
-      {
-        image: "/src/assets/banner-promo01.png",
-        label: "Desde",
-        discount: "40% Dcto",
-        title: "PLANTAS DE INTERIOR"
-      },
-      {
-        image: "/src/assets/banner-promo02.png",
-        discount: "40%",
-        label: "Dcto",
-        title: "PLANTAS DE INTERIOR"
-      },
-      {
-        image: "/src/assets/banner-promo03.jpeg",
-        discount: "40%",
-        label: "Dcto",
-        title: "PLANTAS DE INTERIOR"
-      }
-    ];
-   
-    return (
-      <div className="banner-container">
-        {banners.map((banner, index) => (
-          <Banner key={index} {...banner} />
+  image: string;
+  discount: string;
+  label: string;
+  title: string;
+}
+
+const Banner: React.FC<BannerProps> = ({ image, discount, label, title }) => {
+  return (
+    <div className="banner">
+      <img src={image} alt={title} />
+      <div className="banner-content-promo">
+      <div className="label">{label}</div>
+        <div className="discount">{discount}</div>
+        {title.split(' DE ').map((part, index) => (
+          <div key={index} className="title-line">
+            {index === 0 ? `${part} DE` : part}
+          </div>
         ))}
       </div>
-    );
-   };
-   
-   export default BannerContainer;
+    </div>
+  );
+};
+
+const BannerContainer: React.FC = () => {
+  const banners = [
+    {
+      label: "Desde",
+      discount: "40% Dcto",
+      title: "PLANTAS DE INTERIOR",
+      image: "/src/assets/banner-promo01.png",
+
+    },
+    {
+      label: "Desde",
+      discount: "40% Dcto",
+      title: "PLANTAS DE INTERIOR",
+      image: "/src/assets/banner-promo02.png",
+    },
+    {
+      label: "Desde",
+      discount: "40% Dcto",
+      title: "PLANTAS DE INTERIOR",
+      image: "/src/assets/banner-promo03.jpeg",
+    }
+  ];
+
+  return (
+    <div className="banner-container">
+      {banners.map((banner, index) => (
+        <Banner key={index} {...banner} />
+      ))}
+    </div>
+  );
+};
+
+export default BannerContainer;
